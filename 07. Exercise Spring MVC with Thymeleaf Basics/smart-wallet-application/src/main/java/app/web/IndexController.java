6704aa5888c0f439a9415d899b2e41cab9a2d9f4
Dimitar-Peev <mctrix87@abv.bg>
2025-01-25 19:38:55 +0200

    Update "IndexController.java"

--- 07. Exercise Spring MVC with Thymeleaf Basics/smart-wallet-application/src/main/java/app/web/IndexController.java
+++ 07. Exercise Spring MVC with Thymeleaf Basics/smart-wallet-application/src/main/java/app/web/IndexController.java
@@ -2,13 +2,9 @@ package app.web;
 
 import app.user.model.User;
 import app.user.service.UserService;
-import app.web.dto.RegisterRequest;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.UUID;
@@ -26,36 +22,19 @@ public class IndexController {
     @GetMapping("/")
     public String getIndexPage() {
 
-
         return "index";
     }
 
     @GetMapping("/login")
     public String getLoginPage() {
 
-
         return "login";
     }
 
     @GetMapping("/register")
-    public ModelAndView getRegisterPage() {
+    public String getRegisterPage() {
 
-        ModelAndView modelAndView = new ModelAndView("register");
-        modelAndView.addObject("registerRequest", new RegisterRequest());
-
-        return modelAndView;
-    }
-
-    @PostMapping("/register")
-    public ModelAndView registerNewUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return new ModelAndView("register");
-        }
-
-        userService.register(registerRequest);
-
-        return new ModelAndView("redirect:/home");
+        return "register";
     }
 
     @GetMapping("/home")
