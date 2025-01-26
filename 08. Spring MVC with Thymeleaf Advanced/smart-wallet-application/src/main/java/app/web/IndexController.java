c3215ea7618052da685d8559e4ce2a0649f5569d
Dimitar-Peev <mctrix87@abv.bg>
2025-01-26 09:11:22 +0200

    Add "IndexController.java"

---
+++ 08. Spring MVC with Thymeleaf Advanced/smart-wallet-application/src/main/java/app/web/IndexController.java
@@ -0,0 +1,75 @@
+package app.web;
+
+import app.user.model.User;
+import app.user.service.UserService;
+import app.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Controller
+public class IndexController {
+
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getIndexPage() {
+
+
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+
+
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegisterPage() {
+
+        ModelAndView modelAndView = new ModelAndView("register");
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+//        modelAndView.addObject("countryList", Country.values());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerNewUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+
+            return new ModelAndView("register");
+        }
+
+        userService.register(registerRequest);
+
+        return new ModelAndView("redirect:/home");
+    }
+
+    @GetMapping("/home")
+    public ModelAndView getHomePage() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+
+        User user = userService.getById(UUID.fromString("cd8e1d31-53e4-4cf1-9df6-851a32c613db"));
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+
+}
