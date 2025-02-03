7d30c5dd146839478cd68cadfe5ceda1e3184777
Dimitar-Peev <mctrix87@abv.bg>
2025-02-03 17:55:16 +0200

    Add "UserService.java"

---
+++ Java Web Development Basics - Exams/04. Spring Fundamentals Regular Exam - 21 October 2023/Dictionary/src/main/java/com/example/dictionary/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.dictionary.service;
+
+import com.example.dictionary.model.entity.User;
+import com.example.dictionary.model.service.UserServiceModel;
+
+public interface UserService {
+
+    boolean register(UserServiceModel userServiceModel);
+
+    boolean login(UserServiceModel userServiceModel);
+
+    User findUserByUsername(String username);
+}
