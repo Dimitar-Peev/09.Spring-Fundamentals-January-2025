e5babce68de73faeb535b174f35d403808ded66f
Dimitar-Peev <mctrix87@abv.bg>
2025-02-18 19:20:32 +0200

    Add "Constants.java"

---
+++ Java Web Development Basics - Exams/02. Spring Fundamentals Regular Exam - 23 June 2024/PaintingsCollectors/src/main/java/com/paintingscollectors/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.paintingscollectors.constants;
+
+public class Constants {
+
+    public final static String IMPRESSIONISM = "Impressionism is a painting style most commonly associated with the 19th century where small brush strokes are used to build up a larger picture.";
+    public final static String ABSTRACT = "Abstract art does not attempt to represent recognizable subjects in a realistic manner.";
+    public final static String EXPRESSIONISM = "Expressionism is a style of art that doesn't concern itself with realism.";
+    public final static String SURREALISM = "Surrealism is characterized by dreamlike, fantastical imagery that often defies logical explanation.";
+    public final static String REALISM = "Also known as naturalism, this style of art is considered as 'real art' and has been the dominant style of painting since the Renaissance.";
+
+    public static final String BINDING_MODEL = "org.springframework.validation.BindingResult.";
+}
