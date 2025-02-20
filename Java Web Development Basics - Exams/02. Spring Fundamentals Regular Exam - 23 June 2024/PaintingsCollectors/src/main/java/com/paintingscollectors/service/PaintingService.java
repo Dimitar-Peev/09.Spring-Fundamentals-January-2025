0c76ef26ec5fc784a81c27a5c928d7be43b51dab
Dimitar-Peev <mctrix87@abv.bg>
2025-02-20 17:43:18 +0200

    Add "PaintingService.java"

---
+++ Java Web Development Basics - Exams/02. Spring Fundamentals Regular Exam - 23 June 2024/PaintingsCollectors/src/main/java/com/paintingscollectors/service/PaintingService.java
@@ -0,0 +1,29 @@
+package com.paintingscollectors.service;
+
+import com.paintingscollectors.model.service.PaintingServiceModel;
+import com.paintingscollectors.model.view.HomeViewModel;
+
+import java.util.List;
+
+public interface PaintingService {
+
+    boolean add(PaintingServiceModel paintingServiceModel);
+
+    List<PaintingServiceModel> getMyPaintings(String username);
+
+    void remove(String id);
+
+    List<PaintingServiceModel> getOtherPaintings(String username);
+
+    void addToFavorites(String paintingId, String username);
+
+    void votePainting(String paintingId, String username);
+
+    List<PaintingServiceModel> getUserFavoritePaintings(String username);
+
+    void removeFromFavorites(String paintingId, String username);
+
+    List<PaintingServiceModel> getMostRatedPaintings();
+
+    HomeViewModel getHomeViewModel();
+}
