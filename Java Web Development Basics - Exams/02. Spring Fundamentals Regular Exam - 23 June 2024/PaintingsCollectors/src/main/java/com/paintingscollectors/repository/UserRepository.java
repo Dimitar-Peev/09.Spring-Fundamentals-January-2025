1d4283dad5331490f6378c7dbd2f8a2f99d70d30
Dimitar-Peev <mctrix87@abv.bg>
2025-02-21 21:43:31 +0200

    Update "UserRepository.java"

--- Java Web Development Basics - Exams/02. Spring Fundamentals Regular Exam - 23 June 2024/PaintingsCollectors/src/main/java/com/paintingscollectors/repository/UserRepository.java
+++ Java Web Development Basics - Exams/02. Spring Fundamentals Regular Exam - 23 June 2024/PaintingsCollectors/src/main/java/com/paintingscollectors/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.paintingscollectors.repository;
 
+import com.paintingscollectors.model.entity.Painting;
 import com.paintingscollectors.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsernameOrEmail(String username, String email);
 
     Optional<User> findByUsername(String username);
+
+    List<User> findAllByRatedPaintingsContaining(Painting painting);
 }
