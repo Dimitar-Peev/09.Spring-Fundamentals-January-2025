0de21d9aea3815462fe47ada3b0b656b3b71ba0a
Dimitar-Peev <mctrix87@abv.bg>
2025-02-21 21:45:43 +0200

    Add "WebConfiguration.java"

---
+++ Java Web Development Basics - Exams/02. Spring Fundamentals Regular Exam - 23 June 2024/PaintingsCollectors/src/main/java/com/paintingscollectors/config/WebConfiguration.java
@@ -0,0 +1,26 @@
+package com.paintingscollectors.config;
+
+import com.paintingscollectors.security.SessionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final SessionInterceptor sessionInterceptor;
+
+    @Autowired
+    public WebConfiguration(SessionInterceptor sessionInterceptor) {
+        this.sessionInterceptor = sessionInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(sessionInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/img/**");
+    }
+}
