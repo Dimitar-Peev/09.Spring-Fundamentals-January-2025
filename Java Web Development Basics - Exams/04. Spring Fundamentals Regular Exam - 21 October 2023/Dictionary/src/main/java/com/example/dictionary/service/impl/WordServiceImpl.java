43f897ee1f0f3cbf400a00c456cf7052e3251c21
Dimitar-Peev <mctrix87@abv.bg>
2025-02-24 17:45:12 +0200

    Add "WordServiceImpl.java"

--- Java Web Development Basics - Exams/04. Spring Fundamentals Regular Exam - 21 October 2023/Dictionary/src/main/java/com/example/dictionary/service/impl/WordServiceImpl.java
+++ Java Web Development Basics - Exams/04. Spring Fundamentals Regular Exam - 21 October 2023/Dictionary/src/main/java/com/example/dictionary/service/impl/WordServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.dictionary.model.entity.Language;
 import com.example.dictionary.model.entity.LanguageName;
 import com.example.dictionary.model.entity.Word;
 import com.example.dictionary.model.service.WordServiceModel;
+import com.example.dictionary.model.view.HomeViewModel;
+import com.example.dictionary.model.view.WordViewModel;
 import com.example.dictionary.repository.WordRepository;
 import com.example.dictionary.service.LanguageService;
 import com.example.dictionary.service.UserService;
@@ -43,7 +45,7 @@ public class WordServiceImpl implements WordService {
             log.info("Successfully added word.");
             return true;
         } else {
-            log.info("Failed to add word.");
+            log.warn("Failed to add word.");
             return false;
         }
 
@@ -65,6 +67,39 @@ public class WordServiceImpl implements WordService {
     }
 
     @Override
+    public HomeViewModel getHomeViewModel() {
+        HomeViewModel homeViewModel = new HomeViewModel();
+
+        List<WordViewModel> spanishWords = getWordsByLanguage(LanguageName.SPANISH)
+                .stream()
+                .map(wordServiceModel -> modelMapper.map(wordServiceModel, WordViewModel.class))
+                .toList();
+
+        List<WordViewModel> germanWords = getWordsByLanguage(LanguageName.GERMAN)
+                .stream()
+                .map(wordServiceModel -> modelMapper.map(wordServiceModel, WordViewModel.class))
+                .toList();
+
+        List<WordViewModel> frenchWords = getWordsByLanguage(LanguageName.FRENCH)
+                .stream()
+                .map(wordServiceModel -> modelMapper.map(wordServiceModel, WordViewModel.class))
+                .toList();
+
+        List<WordViewModel> italianWords = getWordsByLanguage(LanguageName.ITALIAN)
+                .stream()
+                .map(wordServiceModel -> modelMapper.map(wordServiceModel, WordViewModel.class))
+                .toList();
+
+        homeViewModel.setSpanishWords(spanishWords);
+        homeViewModel.setFrenchWords(frenchWords);
+        homeViewModel.setItalianWords(italianWords);
+        homeViewModel.setGermanWords(germanWords);
+        homeViewModel.setTotalWordCount(getTotalWordCount());
+
+        return homeViewModel;
+    }
+
+    @Override
     public long getTotalWordCount() {
         return this.wordRepository.count();
     }
@@ -72,11 +107,13 @@ public class WordServiceImpl implements WordService {
     @Override
     public void delete(String id) {
         this.wordRepository.deleteById(id);
+        log.info("Successfully deleted a word.");
     }
 
     @Override
     public void deleteAll() {
         this.wordRepository.deleteAll();
+        log.info("Successfully deleted all words.");
     }
 
 }
