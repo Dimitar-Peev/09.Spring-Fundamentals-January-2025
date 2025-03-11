8c02a4faed6ba54bb815457e2ae2f3706baa96da
Dimitar-Peev <mctrix87@abv.bg>
2025-03-11 18:16:27 +0200

    Update "CategoryService.java"

--- Java Web Development Basics - Exams/14. Java Web Development Basics Exam - 17 December 2019/Andreys/src/main/java/com/example/andreys/service/CategoryService.java
+++ Java Web Development Basics - Exams/14. Java Web Development Basics Exam - 17 December 2019/Andreys/src/main/java/com/example/andreys/service/CategoryService.java
@@ -1,6 +1,11 @@
 package com.example.andreys.service;
 
+import com.example.andreys.model.entity.Category;
+import com.example.andreys.model.entity.CategoryName;
+
 public interface CategoryService {
 
     void initCategories();
+
+    Category findByCategoryName(CategoryName name);
 }
