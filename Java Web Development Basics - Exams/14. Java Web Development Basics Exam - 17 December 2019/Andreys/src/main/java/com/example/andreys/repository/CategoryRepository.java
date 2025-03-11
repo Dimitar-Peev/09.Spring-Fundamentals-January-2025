26e4de4442a8ffbf7bc4cb92a3cc883fef934e85
Dimitar-Peev <mctrix87@abv.bg>
2025-03-11 18:16:04 +0200

    Update "CategoryRepository.java"

--- Java Web Development Basics - Exams/14. Java Web Development Basics Exam - 17 December 2019/Andreys/src/main/java/com/example/andreys/repository/CategoryRepository.java
+++ Java Web Development Basics - Exams/14. Java Web Development Basics Exam - 17 December 2019/Andreys/src/main/java/com/example/andreys/repository/CategoryRepository.java
@@ -1,10 +1,14 @@
 package com.example.andreys.repository;
 
 import com.example.andreys.model.entity.Category;
+import com.example.andreys.model.entity.CategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
 
+    Optional<Category> findCategoryByName(CategoryName name);
 }
