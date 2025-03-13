606c165ef9ac02183f7f0de000ffc9df9bb6c126
Dimitar-Peev <mctrix87@abv.bg>
2025-03-13 22:20:45 +0200

    Add "User.java"

---
+++ Java Web Development Basics - Exams/11. Spring Fundamentals Retake Exam - 7 April 2021/CoffeeShop/src/main/java/coffeeshop/model/entity/User.java
@@ -0,0 +1,33 @@
+package coffeeshop.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "employee", fetch = FetchType.EAGER)
+    private Set<Order> orders;
+}
