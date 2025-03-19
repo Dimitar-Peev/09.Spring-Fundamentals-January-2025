3aac7eee255f113bddedb28642a1931ec49bddbe
Dimitar-Peev <mctrix87@abv.bg>
2025-03-19 22:09:21 +0200

    Add "OrderRepository.java"

---
+++ Java Web Development Basics - Exams/11. Spring Fundamentals Retake Exam - 7 April 2021/CoffeeShop/src/main/java/coffeeshop/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package coffeeshop.repository;
+
+import coffeeshop.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, String> {
+
+    List<Order> findAllByOrderByPriceDesc();
+}
