7a053d88736af2d00bed651179adc1f7748ced4c
Dimitar-Peev <mctrix87@abv.bg>
2025-05-13 19:44:37 +0300

    Updated "UserServiceTest.java" with new Unit test.

--- 16. Retake Exam/SoftChat/src/test/java/app/user/service/UserServiceTest.java
+++ 16. Retake Exam/SoftChat/src/test/java/app/user/service/UserServiceTest.java
@@ -160,5 +160,25 @@ public class UserServiceTest {
         assertThrows(RuntimeException.class, () -> userService.login(loginRequest));
     }
 
+    @Test
+    void login_ShouldThrowException_WhenPasswordIsIncorrect() {
+
+        // Given
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("test_user2");
+        loginRequest.setPassword("password123");
+
+        User user = User.builder()
+                .id(UUID.randomUUID())
+                .username("test_user1")
+                .password("encodedPassword")
+                .build();
+
+        when(userRepository.findByUsername(loginRequest.getUsername())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())).thenReturn(false);
+
+        // When & Then
+        assertThrows(RuntimeException.class, () -> userService.login(loginRequest));
+    }
 
 }
